53f564fb1634aa589ab68a184712463220f64190
zhaoqiyuan <zhaoqiyuan@users.noreply.github.com>
2017-02-13 11:04:34 +0800

    Update proxy.py

--- 3.x/proxy.py
+++ 3.x/proxy.py
@@ -2,3 +2,7 @@ import urllib.request
 proxy_handler = urllib.request.ProxyHandler({'http':'username:password@proxy:port'})
 opener = urllib.request.build_opener(proxy_handler)
 urllib.request.install_opener(opener)
+
+resp = urllib.request.urlopen("http://www.baidu.com")
+data = resp.read()
+print(len(data))
