f5f9f329fd6ee5d8be835930453d2dbbca96b28a
zhaoqiyuan <zhaoqiyuan@users.noreply.github.com>
2017-02-13 10:55:28 +0800

    Create proxy.py

---
+++ 3.x/proxy.py
@@ -0,0 +1,4 @@
+import urllib.request
+proxy_handler = urllib.request.ProxyHandler({'http':'username:password@proxy:port'})
+opener = urllib.request.build_opener(proxy_handler)
+urllib.request.install_opener(opener)
